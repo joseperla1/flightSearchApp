781891d9a98acecc62d125592c56c694ce39d6a2
Jose <perlajose833@gmail.com>
2024-02-11 02:13:41 -0600

    header component

--- FlightSearchApp/src/App.jsx
+++ FlightSearchApp/src/App.jsx
@@ -3,11 +3,14 @@ import { useState } from 'react'
 //import './styles/App.css'
 import script from './script/script'
 import Results from './components/Results'
+import Header from './components/Header'
 function App() {
 
   return (
     <>
+    <Header/>
     <div className='card-grid'>
+      <Results/>
       <Results/>
       <Results/>
     </div>
